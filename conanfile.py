ec5761d4ac6a5c6028424fa34b186fd7b1a100f7
Fred Malouf <fred.malouf@nio.io>
2019-09-09 11:21:43 -0700

    Working conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,26 @@
-from conans import ConanFile, CMake, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-class DltConan(ConanFile):
-    name = "dlt"
-    version = "2.18.3"
-    license = "https://github.com/GENIVI/dlt-daemon/blob/master/LICENSE"
-    author = "https://github.com/GENIVI/dlt-daemon/graphs/contributors"
-    url = "https://github.com/GENIVI/dlt-daemon.git"
-    description = "GENIVI Diagnostic Log and Trace"
-    topics = ("dlt", "logging", "C++", "trace")
+import os
+from conans import ConanFile, CMake, AutoToolsBuildEnvironment, tools
+
+class EnetConan(ConanFile):
+    name = "enet"
+    version = "1.3.14"
+    license = "https://github.com/lsalzman/enet/blob/master/LICENSE"
+    author = "https://github.com/lsalzman/enet/graphs/contributors"
+    url = "https://github.com/lsalzman/enet.git"
+    description = "ENet reliable UDP networking library"
+    topics = ("udp", "C", "networking")
     settings = "os", "compiler", "build_type", "arch"
     exports = "*"
     options = {
         "shared": [ True, False ],
         "fPIC": [ True, False ],
-        "enable_examples": [ True, False ],
-        "dlt_ipc" : [ "FIFO", "UNIX_SOCKET" ]
     }
     default_options = {
         'shared': True,
         'fPIC': True,
-        'enable_examples': False,
-        'dlt_ipc': "UNIX_SOCKET"
     }
     generators = "cmake"
     source_subfolder = "source_subfolder"
@@ -38,30 +38,31 @@ class DltConan(ConanFile):
 
     def source(self):
         self.run("git clone %s %s" % (self.source_url, self.name))
-        self.run("cd %s && git checkout tags/v%s" % (self.name, self.version))
-
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["WITH_DLT_EXAMPLES"] = self.options.enable_examples
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.definitions["DLT_IPC"] = self.options.dlt_ipc
-        cmake.definitions["WITH_DLT_CXX11_EXT"] = "ON"
-        cmake.definitions["WITH_DLT_USE_IPv6"] = "OFF"
-        if self.settings.os == "QNX":
-        	cmake.definitions["__EXT_BSD"] = "ON"
-        cmake.configure(source_folder=self.name)
-        return cmake
+        self.run("cd %s && git checkout" % (self.name))
 
     def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.fpic = self.options.fPIC
+        #env_build.libs.append("pthread")
+        if self.settings.os == "QNX":
+            env_build.defines.append("__EXT_BSD=ON")
+        with tools.environment_append(env_build.vars):
+            self.run("cd %s && autoreconf -vfi" % (self.name))
+            self.run("cd %s && ./configure" % (self.name))
+            self.run("cd %s && make" % (self.name))
+            self.run("cd %s && ./libtool --mode=install /usr/bin/install -c libenet.la %s/%s" % (self.name, self.build_folder, self.name))
 
     def package(self):
-        cmake = self.configure_cmake()
-        cmake.install()
+        self.copy(pattern="LICENSE", dst="licenses", src="%s/%s" % (self.build_folder, self.name))
+        self.copy("*.h", "include", "%s/%s/include" % (self.build_folder, self.name))
+        if self.options.shared:
+            self.copy(pattern="*.dll", dst="bin", keep_path=False)
+            self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+            self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        else:
+            self.copy(pattern="*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["dlt"]
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Windows":
             self.cpp_info.libs.extend(['psapi', 'ws2_32'])
