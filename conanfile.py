82f3eab0a75c0ccc30211cccfd871473b3ac463e
Fred Malouf <fred.malouf@nio.io>
2019-09-10 15:23:05 -0700

    Use cmake

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
-from conans import ConanFile, CMake, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, CMake, tools
 
 class EnetConan(ConanFile):
     name = "enet"
     version = "1.3.14"
-    license = "https://github.com/lsalzman/enet/blob/master/LICENSE"
-    author = "https://github.com/lsalzman/enet/graphs/contributors"
-    url = "https://github.com/lsalzman/enet.git"
+    license = "https://github.com/maingig/enet/blob/master/LICENSE"
+    author = "https://github.com/maingig/enet/graphs/contributors"
+    url = "https://github.com/maingig/enet.git"
     description = "ENet reliable UDP networking library"
     topics = ("udp", "C", "networking")
     settings = "os", "compiler", "build_type", "arch"
@@ -19,7 +19,7 @@ class EnetConan(ConanFile):
         "fPIC": [ True, False ],
     }
     default_options = {
-        'shared': True,
+        'shared': False,
         'fPIC': True,
     }
     generators = "cmake"
@@ -40,25 +40,17 @@ class EnetConan(ConanFile):
         self.run("git clone %s %s" % (self.source_url, self.name))
         self.run("cd %s && git checkout" % (self.name))
 
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self.name, build_folder=self.name)
+        return cmake
+
     def build(self):
-        env_build = AutoToolsBuildEnvironment(self)
-        env_build.fpic = self.options.fPIC
-        #env_build.libs.append("pthread")
-        if self.settings.os == "QNX":
-            env_build.defines.append("__EXT_BSD=ON")
-        if self.settings.os == "Windows" and self.options.shared:
-            env_build.defines.append("ENET_DLL=ON")
-        with tools.environment_append(env_build.vars):
-            if self.settings.os == "Windows":
-                self.run('cd %s && NMAKE /f "enet.mak" CFG="enet - Win32 Debug"' % (self.name))
-            else:
-                self.run("cd %s && autoreconf -vfi" % (self.name))
-                self.run("cd %s && ./configure" % (self.name))
-                self.run("cd %s && make" % (self.name))
-                self.run("cd %s && ./libtool --mode=install /usr/bin/install -c libenet.la %s/%s" % (self.name, self.build_folder, self.name))
+        cmake = self.configure_cmake()
+        cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src="%s/%s" % (self.build_folder, self.name))
+        self.copy(pattern="COPYING", dst="licenses", src=self.source_subfolder)
         self.copy("*.h", "include", "%s/%s/include" % (self.build_folder, self.name))
         if self.options.shared:
             self.copy(pattern="*.dll", dst="bin", keep_path=False)
@@ -66,6 +58,7 @@ class EnetConan(ConanFile):
             self.copy(pattern="*.so*", dst="lib", keep_path=False)
         else:
             self.copy(pattern="*.a", dst="lib", keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
